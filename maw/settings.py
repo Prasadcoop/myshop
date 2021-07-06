5fabb64d142005d41dc34138af4f0179eea0930f
Prasadcoop <prasad.sangale11@gmail.com>
2021-07-06 12:18:13 +0530

    djangotest

--- maw/settings.py
+++ maw/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-42ffk2_lzi#!#8w#mqz)!_2ui%btz8+rh&%waq+@klms!d'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'passgenpk.herokuapp.com'
+
 ]
 
 
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'maw.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR ,'db.sqlite3',)
+        'NAME': BASE_DIR /'db.sqlite3',
     }
 }
 
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/ 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
